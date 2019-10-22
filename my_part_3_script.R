31ee0fa19d2a88e671a6018010e6831d6e1861e8
peytonaileen <pamoore@ucsb.edu>
2019-10-22 14:00:44 -0700

    adding disease mortality graph and script

---
+++ my_part_3_script.R
@@ -0,0 +1,24 @@
+# Peyton Moore 
+
+library(tidyverse)
+library(here)
+library(janitor)
+
+
+db <- read_csv(here::here("data","disease_burden.csv")) %>% 
+  clean_names() %>% 
+  rename(deaths_per_100k = death_rate_per_100_000)
+
+
+db_sub <- db %>% 
+  filter(country_name %in% c("United States", "Japan", "Afghanistan", "Somalia")) %>% 
+  filter(age_group == "0-6 days", sex == "Both")
+
+
+#Graph:
+
+ggplot(data = db_sub, aes(x = year, y = deaths_per_100k))+
+  geom_line(aes(color = country_name))
+
+
+ggsave(here::here("final_graphs", "disease_graphs.png"))
